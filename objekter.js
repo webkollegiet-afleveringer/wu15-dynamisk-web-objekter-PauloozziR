5afbed84b997354f483058089fadf4358b617642
Paulo <paulorzzbj@gmail.com>
2026-02-03 10:19:42 +0100

    opgave 2

--- objekter.js
+++ objekter.js
@@ -3,15 +3,35 @@ let person = {
     firstname: "Ole",
     lastname: "Erling",
     age: 54,
-    hometown: "Roskilde" ,
-    pet: "Klapperslange" ,
+    hometown: "Roskilde",
+    pet: "Klapperslange",
     petname: "Holger"
 }
-console.log(person.firstname);
-console.log(person.lastname);
-console.log(person.age);
-console.log(person["hometown"]);
-console.log(person["pet"]);
-console.log(person["petname"]);
+console.log("Opgave 1", person.firstname);
+console.log("Opgave 1", person.lastname);
+console.log("Opgave 1", person.age);
+console.log("Opgave 1", person["hometown"]);
+console.log("Opgave 1", person["pet"]);
+console.log("Opgave 1", person["petname"]);
 
 // --- Opgave 2 ---
+let person2 = {
+    firstname: "Ole",
+    lastname: "Erling",
+    married: true,
+    age: 54,
+    hometown: "Roskilde",
+    hobbies: ["Reptiler", "Frimærker", "Programmering"],
+    pet: { 
+        type: "Klapperslange",
+        name: "Holger"
+    },
+}
+person2.hobbies.forEach(function(hobby) {
+    console.log("Opgave 2", hobby);
+})
+console.log("Opgave 2", person2.pet["type"]);
+console.log("Opgave 2", person2.pet.name);
+
+// --- Opgave 3 ---
+console.log("Opgave 3", );
