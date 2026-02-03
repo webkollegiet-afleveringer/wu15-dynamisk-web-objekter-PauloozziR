52a2dad1fd2c98d45b3e7900f410552a3cfe8c32
Paulo <paulorzzbj@gmail.com>
2026-02-03 09:27:27 +0100

    opgave 1

---
+++ objekter.js
@@ -0,0 +1,17 @@
+// --- Opgave 1 ---
+let person = {
+    firstname: "Ole",
+    lastname: "Erling",
+    age: 54,
+    hometown: "Roskilde" ,
+    pet: "Klapperslange" ,
+    petname: "Holger"
+}
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.age);
+console.log(person["hometown"]);
+console.log(person["pet"]);
+console.log(person["petname"]);
+
+// --- Opgave 2 ---
