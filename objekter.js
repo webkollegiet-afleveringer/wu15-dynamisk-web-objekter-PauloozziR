03a3455f17e73aeae43a3ee81ebe891ba502bd06
Paulo <paulorzzbj@gmail.com>
2026-02-03 11:21:51 +0100

    opgave 3 minus $

--- objekter.js
+++ objekter.js
@@ -34,4 +34,21 @@ console.log("Opgave 2", person2.pet["type"]);
 console.log("Opgave 2", person2.pet.name);
 
 // --- Opgave 3 ---
-console.log("Opgave 3", );
+let students = [
+    { name: "Sofie", age: 25, course: "Webudvikler"},
+    { name: "Lukas", age: 22, course: "Mediegrafiker"},
+    { name: "Emil", age: 28, course: "Teknisk designer"},
+];
+let = studentListDiv = document.querySelector(".student-list");
+students.forEach(function(student) {
+    const studentDiv = document.createElement("div");
+    studentDiv.textContent = "Navn: " + student.name + ", Alder: " + student.age + ", Uddannelse: " + student.course;
+    studentListDiv.append(studentDiv);
+});
+
+/* students.forEach(student => {
+    let studentDiv = document.createElement("div");
+    studentDiv.textContent = "Navn: ${student.name}, Alder: ${student.age}, Uddannelse: ${student.course}";
+    studentListDiv.appendChild(studentDiv);
+}); */
+console.log("Opgave 3", studentListDiv);
